1a952cb87e94a7a8925abbdf01c5433e4c061c72
OnoFumiya <fumiyaono.choi@gmail.com>
2026-04-25 00:05:35 +0900

    add interfaces and library

---
+++ nekomimi_bot_library/src/nekomimi_bot_joint_action_server.cpp
@@ -0,0 +1,419 @@
+#include "nekomimi_bot_library/nekomimi_bot_joint_action_server.hpp"
+
+namespace nekomimi_bot{
+
+JointActionServer::JointActionServer(const rclcpp::NodeOptions & options = rclcpp::NodeOptions())
+: Node("joint_action_server", options),
+  tf_buffer_(std::make_shared<tf2_ros::Buffer>(this->get_clock())),
+  tf_listener_(std::make_shared<tf2_ros::TransformListener>(*tf_buffer_))
+{
+  // Configure the QoS profile
+  rclcpp::QoS qos_profile(1); // depth = 1
+  // qos_profile.reliability(RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT);
+  qos_profile.reliability(RMW_QOS_POLICY_RELIABILITY_RELIABLE);
+  qos_profile.history(RMW_QOS_POLICY_HISTORY_KEEP_LAST);
+  qos_profile.durability(RMW_QOS_POLICY_DURABILITY_VOLATILE);
+
+
+  this->action_server_move_joints_ = rclcpp_action::create_server<MoveJoint>(
+      this,
+      "move_joint",
+      std::bind(&JointActionServer::handle_move_joints_goal, this, std::placeholders::_1, std::placeholders::_2),
+      std::bind(&JointActionServer::handle_move_joints_cancel, this, std::placeholders::_1),
+      std::bind(&JointActionServer::handle_move_joints_accepted, this, std::placeholders::_1));
+  this->action_server_move_to_pose_ = rclcpp_action::create_server<MoveToPose>(
+      this,
+      "move_to_pose",
+      std::bind(&JointActionServer::handle_move_to_pose_goal, this, std::placeholders::_1, std::placeholders::_2),
+      std::bind(&JointActionServer::handle_move_to_pose_cancel, this, std::placeholders::_1),
+      std::bind(&JointActionServer::handle_move_to_pose_accepted, this, std::placeholders::_1));
+  this->service_server_get_hand_to_coord_ = this->create_service<GetHandToTargetCoord>(
+      "get_hand_to_coord",
+      std::bind(&JointActionServer::serve_get_hand_to_coord, this, std::placeholders::_1, std::placeholders::_2));
+  this->service_server_get_hand_to_tf_ = this->create_service<GetHandToTargetTF>(
+      "get_hand_to_tf",
+      std::bind(&JointActionServer::serve_get_hand_to_tf, this, std::placeholders::_1, std::placeholders::_2));
+
+  this->sub_joint_state_ = this->create_subscription<sensor_msgs::msg::JointState>(
+      "joint_states", qos_profile, std::bind(&JointActionServer::joint_state_callback, this, std::placeholders::_1));
+  this->pub_head_joint_control_ = this->create_publisher<trajectory_msgs::msg::JointTrajectory>(
+      "joint_trajectory_controller/joint_trajectory", qos_profile);
+
+
+  //Declare the pose parameters
+
+  this->declare_parameter("poses", std::vector<std::string>());
+  auto pose_names = this->get_parameter("poses").as_string_array();
+
+  poses_.clear();
+  for (auto pose_name : pose_names) {
+    // Declare parameters for each pose
+    this->declare_parameter(pose_name + ".head_pan"  , rclcpp::PARAMETER_DOUBLE);
+    this->declare_parameter(pose_name + ".head_tilt" , rclcpp::PARAMETER_DOUBLE);
+
+    // Read parameters for each pose
+    PoseParams params;
+    params.pose_name       = pose_name;
+    params.head_pan_joint  = this->get_parameter(pose_name + ".head_pan").as_double();
+    params.head_tilt_joint = this->get_parameter(pose_name + ".head_tilt").as_double();
+
+    poses_.push_back(params);
+  }
+
+  RCLCPP_INFO(this->get_logger(), "JointActionServer has been initialized.");
+}
+JointActionServer::~JointActionServer()
+{
+  this->action_server_move_joints_.reset();
+  this->action_server_move_to_pose_.reset();
+
+  this->sub_joint_state_.reset();
+  this->pub_head_joint_control_.reset();
+
+  RCLCPP_INFO(this->get_logger(), "JointActionServer has been terminated.");
+}
+
+
+rclcpp_action::GoalResponse JointActionServer::handle_move_joints_goal(
+  const rclcpp_action::GoalUUID & uuid,
+  std::shared_ptr<const MoveJoint::Goal> goal)
+{
+  RCLCPP_INFO(this->get_logger(), "Received goal request");
+  (void)uuid;
+  (void)goal;
+  return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+}
+
+rclcpp_action::GoalResponse JointActionServer::handle_move_to_pose_goal(
+  const rclcpp_action::GoalUUID & uuid,
+  std::shared_ptr<const MoveToPose::Goal> goal)
+{
+  RCLCPP_INFO(this->get_logger(), "Received goal request");
+  (void)uuid;
+  (void)goal;
+  return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+}
+
+
+rclcpp_action::CancelResponse JointActionServer::handle_move_joints_cancel(
+  const std::shared_ptr<GoalHandleMoveJoints> goal_handle)
+{
+  RCLCPP_INFO(this->get_logger(), "Received cancel request");
+  (void)goal_handle;
+  return rclcpp_action::CancelResponse::ACCEPT;
+}
+rclcpp_action::CancelResponse JointActionServer::handle_move_to_pose_cancel(
+  const std::shared_ptr<GoalHandleMoveToPose> goal_handle)
+{
+  RCLCPP_INFO(this->get_logger(), "Received cancel request");
+  (void)goal_handle;
+  return rclcpp_action::CancelResponse::ACCEPT;
+}
+
+
+void JointActionServer::handle_move_joints_accepted(
+  const std::shared_ptr<GoalHandleMoveJoints> goal_handle)
+{
+  RCLCPP_INFO(this->get_logger(), "Received goal request");
+  (void)goal_handle;
+  std::thread{std::bind(&JointActionServer::exe_move_joints, this, std::placeholders::_1), goal_handle}.detach();
+}
+
+void JointActionServer::handle_move_to_pose_accepted(
+  const std::shared_ptr<GoalHandleMoveToPose> goal_handle)
+{
+  RCLCPP_INFO(this->get_logger(), "Received goal request");
+  (void)goal_handle;
+  std::thread{std::bind(&JointActionServer::exe_move_to_pose, this, std::placeholders::_1), goal_handle}.detach();
+}
+
+
+void JointActionServer::exe_move_joints(
+  const std::shared_ptr<GoalHandleMoveJoints> goal_handle)
+{
+  RCLCPP_INFO(this->get_logger(), "Executing goal");
+
+  const auto goal = goal_handle->get_goal();
+  auto result = std::make_shared<MoveJoint::Result>();
+
+  // Check if the number of joint names and joint rad are the same
+  if (goal->target_joint_names.size() != goal->target_joint_rad.size()) {
+    RCLCPP_ERROR(this->get_logger(), "Invalid goal request. The number of joint names and joint rad are different");
+    result->success = false;
+    result->message = "Invalid goal request. The number of joint names and joint rad are different";
+    result->total_elapsed_time.sec = 0;
+    result->total_elapsed_time.nanosec = 0;
+    goal_handle->abort(result);
+    return;
+  }
+
+  // Check if the joint names are valid
+  for (size_t i = 0; i < goal->target_joint_names.size(); i++) {
+    if (std::find(JointNames.begin(), JointNames.end(), goal->target_joint_names[i]) == JointNames.end() &&
+        std::find(JointNamesHand.begin(), JointNamesHand.end(), goal->target_joint_names[i]) == JointNamesHand.end()) {
+      RCLCPP_ERROR(this->get_logger(), "The joint name does not exist: %s", goal->target_joint_names[i].c_str());
+      result->success = false;
+      result->message = "The joint name does not exist: " + goal->target_joint_names[i];
+      result->total_elapsed_time.sec = 0;
+      result->total_elapsed_time.nanosec = 0;
+      goal_handle->abort(result);
+      return;
+    }
+  }
+
+  // Publish the joint trajectory
+  trajectory_msgs::msg::JointTrajectory head_joint_trajectory;
+  head_joint_trajectory = set_joints(goal->target_joint_names, goal->target_joint_rad, goal->time_allowance, "head");
+
+  try {
+    if (!head_joint_trajectory.joint_names.empty())
+      this->pub_head_joint_control_->publish(head_joint_trajectory);
+  } catch (const std::exception &ex) {
+    RCLCPP_ERROR(this->get_logger(), "Failed to publish the joint trajectory: %s", ex.what());
+
+    result->success = false;
+    result->message = "[FAIL] Failed to publish the joint trajectory";
+    result->total_elapsed_time.sec = 0;
+    result->total_elapsed_time.nanosec = 0;
+    goal_handle->abort(result);
+
+    return;
+  }
+
+  // Publish feedback
+  auto start_time = this->now();
+
+  while (this->now() - start_time < goal->time_allowance) {
+    if (goal_handle->is_canceling()) {
+      RCLCPP_INFO(this->get_logger(), "Goal has been canceled");
+
+      result->success = false;
+      result->message = "[CANCEL] Goal has been canceled";
+      result->total_elapsed_time.sec = (this->now() - start_time).seconds();
+      result->total_elapsed_time.nanosec = (this->now() - start_time).nanoseconds() % int(10E9);
+      goal_handle->canceled(result);
+
+      // TODO: Stop of current joint...
+      // builtin_interfaces::msg::Duration dt;
+      // dt.sec = 0;
+      // dt.nanosec = static_cast<uint32_t>(0.1 * 10E9);
+      // this->pub_joint_control_->publish(set_joints({}, {}, dt));
+
+      return;
+    }
+
+    auto feedback = std::make_shared<MoveJoint::Feedback>();
+    feedback->current_joint_names = goal->target_joint_names;
+    for (const auto &joint_name : goal->target_joint_names) {
+      feedback->current_joint_rad.push_back(this->curt_joint_state_[joint_name]);
+    }
+    feedback->move_time.sec = (this->now() - start_time).seconds();
+    feedback->move_time.nanosec = (this->now() - start_time).nanoseconds() % int(10E9);
+
+    goal_handle->publish_feedback(feedback);
+
+  }
+
+  // Check if goal was reached
+  for (size_t i = 0; i < goal->target_joint_names.size(); i++) {
+    // TODO: set tolerance with parameter or msg
+    if (std::abs(this->curt_joint_state_[goal->target_joint_names[i]] - goal->target_joint_rad[i]) > 0.1) {
+      RCLCPP_ERROR(this->get_logger(), "Failed to reach the goal");
+
+      result->success = false;
+      result->message = "[FAIL] Failed to reach the goal";
+      result->total_elapsed_time.sec = (this->now() - start_time).seconds();
+      result->total_elapsed_time.nanosec = (this->now() - start_time).nanoseconds() % int(10E9);
+      goal_handle->abort(result);
+
+      return;
+    }
+  }
+
+  // Clear the current joint state
+  curt_joint_state_.clear();
+
+  // Publish the result
+  result->success = true;
+  result->message = "Goal has been succeeded";
+  result->total_elapsed_time.sec = (this->now() - start_time).seconds();
+  result->total_elapsed_time.nanosec = (this->now() - start_time).nanoseconds() % int(10E9);
+
+  goal_handle->succeed(result);
+}
+
+void JointActionServer::exe_move_to_pose(
+  const std::shared_ptr<GoalHandleMoveToPose> goal_handle)
+{
+  RCLCPP_INFO(this->get_logger(), "Executing goal");
+
+  const auto goal = goal_handle->get_goal();
+  auto result = std::make_shared<MoveToPose::Result>();
+
+  // Check if the pose name is valid
+  if (std::find_if(poses_.begin(), poses_.end(), [&](const PoseParams &pose) { return pose.pose_name == goal->pose_name; }) == poses_.end()) {
+    RCLCPP_ERROR(this->get_logger(), "Invalid pose name: %s", goal->pose_name.c_str());
+    result->success = false;
+    result->message = "Invalid pose name: " + goal->pose_name;
+    result->total_elapsed_time.sec = 0;
+    result->total_elapsed_time.nanosec = 0;
+    goal_handle->abort(result);
+    return;
+  }
+
+  // Get the target joint rad from the pose name
+  std::vector<double> target_joint_rad;
+  for (const auto &pose : poses_) {
+    if (pose.pose_name == goal->pose_name) {
+      target_joint_rad.push_back(pose.head_pan_joint);
+      target_joint_rad.push_back(pose.head_tilt_joint);
+      break;
+    }
+  }
+
+  if (target_joint_rad.size() == 0) {
+    RCLCPP_ERROR(this->get_logger(), "Failed to not find the pose name : %s", goal->pose_name.c_str());
+
+    result->success = false;
+    result->message = "[FAIL] Failed to not find the pose name : " +  goal->pose_name;
+    result->total_elapsed_time.sec = 0;
+    result->total_elapsed_time.nanosec = 0;
+    goal_handle->abort(result);
+  }
+
+  initial_joint_state_ = curt_joint_state_;
+
+
+  // Publish the joint trajectory
+  trajectory_msgs::msg::JointTrajectory head_joint_trajectory;
+  head_joint_trajectory = set_joints(JointNames, target_joint_rad, goal->time_allowance, "head");
+
+  try {
+    if (!head_joint_trajectory.joint_names.empty())
+      this->pub_head_joint_control_->publish(head_joint_trajectory);
+  } catch (const std::exception &ex) {
+    RCLCPP_ERROR(this->get_logger(), "Failed to publish the joint trajectory: %s", ex.what());
+
+    result->success = false;
+    result->message = "[FAIL] Failed to publish the joint trajectory";
+    result->total_elapsed_time.sec = 0;
+    result->total_elapsed_time.nanosec = 0;
+    goal_handle->abort(result);
+
+    return;
+  }
+
+  // Publish feedback
+  auto start_time = this->now();
+
+  while (this->now() - start_time < goal->time_allowance) {
+    if (goal_handle->is_canceling()) {
+      RCLCPP_INFO(this->get_logger(), "Goal has been canceled");
+
+      result->success = false;
+      result->message = "[CANCEL] Goal has been canceled";
+      result->total_elapsed_time.sec = (this->now() - start_time).seconds();
+      result->total_elapsed_time.nanosec = (this->now() - start_time).nanoseconds() % int(10E9);
+      goal_handle->canceled(result);
+
+      // TODO: Stop of current joint...
+      // builtin_interfaces::msg::Duration dt;
+      // dt.sec = 0;
+      // dt.nanosec = static_cast<uint32_t>(0.1 * 10E9);
+      // this->pub_joint_control_->publish(set_joints({}, {}, dt));
+  
+      return;
+    }
+
+    auto feedback = std::make_shared<MoveToPose::Feedback>();
+    feedback->current_joint_names = JointNames;
+    for (const auto &joint_name : JointNames) {
+      feedback->current_joint_rad.push_back(this->curt_joint_state_[joint_name]);
+    }
+    feedback->move_time.sec = (this->now() - start_time).seconds();
+    feedback->move_time.nanosec = (this->now() - start_time).nanoseconds() % int(10E9);
+
+    goal_handle->publish_feedback(feedback);
+  }
+
+  // Check if goal was reached
+  for (size_t i = 0; i < JointNames.size(); i++) {
+    // TODO: set tolerance with parameter or msg
+      double diff = std::abs(this->curt_joint_state_[JointNames[i]] - target_joint_rad[i]);
+      double moved = std::abs(this->curt_joint_state_[JointNames[i]] - initial_joint_state_[JointNames[i]]);
+
+      if (diff > 0.1) {
+        if (moved < 0.01) {
+          RCLCPP_ERROR(this->get_logger(),
+            "Joint %s did not move! initial=%.3f, target=%.3f, final=%.3f",
+            JointNames[i].c_str(),
+            initial_joint_state_[JointNames[i]],
+            target_joint_rad[i],
+            this->curt_joint_state_[JointNames[i]]);
+        } else {
+          RCLCPP_ERROR(this->get_logger(),
+            "Joint %s failed to reach target. moved=%.3f, target=%.3f, final=%.3f, diff=%.3f",
+            JointNames[i].c_str(),
+            moved,
+            target_joint_rad[i],
+            this->curt_joint_state_[JointNames[i]],
+            diff);
+        }
+
+      result->success = false;
+      result->message = "[FAIL] Failed to reach the goal";
+      result->total_elapsed_time.sec = (this->now() - start_time).seconds();
+      result->total_elapsed_time.nanosec = (this->now() - start_time).nanoseconds() % int(10E9);
+      goal_handle->abort(result);
+
+      return;
+    }
+  }
+
+  // Clear the current joint state
+  curt_joint_state_.clear();
+
+  // Publish the result
+  result->message = "[SUCCESS] Goal has been succeeded";
+  result->success = true;
+  result->total_elapsed_time.sec = (this->now() - start_time).seconds();
+  result->total_elapsed_time.nanosec = (this->now() - start_time).nanoseconds() % int(10E9);
+
+  goal_handle->succeed(result);
+}
+
+void JointActionServer::joint_state_callback(
+  const sensor_msgs::msg::JointState::SharedPtr msg)
+{
+  for (size_t i = 0; i < msg->name.size(); i++)
+    this->curt_joint_state_[msg->name[i]] = msg->position[i];
+
+}
+
+trajectory_msgs::msg::JointTrajectory JointActionServer::set_joints(
+  const std::vector<std::string> &target_joint_names,
+  const std::vector<double> &target_joint_rad,
+  const builtin_interfaces::msg::Duration &time_allowance,
+  const std::string &group_name)
+{
+  trajectory_msgs::msg::JointTrajectory joint_trajectory;
+  trajectory_msgs::msg::JointTrajectoryPoint point;
+
+  for (size_t i = 0; i < target_joint_names.size(); i++) {
+    // Check if the joint belongs to the specified group
+    if (group_name == "head" &&
+        std::find(JointNamesHead.begin(), JointNamesHead.end(), target_joint_names[i]) == JointNamesHead.end()) {
+      continue;
+    }
+
+    joint_trajectory.joint_names.push_back(target_joint_names[i]);
+    point.positions.push_back(target_joint_rad[i]);
+  }
+
+  joint_trajectory.points.push_back(point);
+  joint_trajectory.points[0].time_from_start = time_allowance;
+
+  return joint_trajectory;
+}
+
+} // namespace nekomimi_bot
