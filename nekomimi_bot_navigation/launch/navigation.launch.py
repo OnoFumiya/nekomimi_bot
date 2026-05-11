23c53e06007702197d5309c387a88310004438cf
OnoFumiya <fumiyaono.choi@gmail.com>
2026-05-11 22:31:21 +0900

    Update navigation configured

--- nekomimi_bot_navigation/launch/navigation.launch.py
+++ nekomimi_bot_navigation/launch/navigation.launch.py
@@ -48,7 +48,7 @@ def generate_launch_description():
     map_file = os.path.join(get_package_share_directory('nekomimi_bot_navigation'), 'map', 'sakura201.yaml')
 
     # Location File Path
-    location_file = os.path.join(get_package_share_directory('nekomimi_bot_navigation'), 'location', 'location_example.yaml')
+    location_file = os.path.join(get_package_share_directory('nekomimi_bot_navigation'), 'location', 'sakura201_location.yaml')
 
     # Use Gazebo
     use_gazebo = False
@@ -63,6 +63,9 @@ def generate_launch_description():
 
     # Pan-Tilt Movement Config
     use_pantilt_move = True
+
+    # Options...
+    use_omnidirectional = False
 
     #####################################################################################
 
@@ -79,7 +82,7 @@ def generate_launch_description():
     use_rviz = LaunchConfiguration('use_rviz')  # Customize
     use_sim_time = LaunchConfiguration('use_sim_time')
     params_file = PathJoinSubstitution(
-        [FindPackageShare('nekomimi_bot_navigation'), 'config', 'nav2_config.yaml']
+        [FindPackageShare('nekomimi_bot_navigation'), 'config', 'nav2_config_for_omni.yaml' if use_omnidirectional else 'nav2_config.yaml']
     )
     slamtool_param_file = PathJoinSubstitution(
         [FindPackageShare('nekomimi_bot_navigation'), 'config', 'slam_config.yaml']
